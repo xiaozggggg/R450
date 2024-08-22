70b685314c57b6aed8b4c82e841f4415a6516cd0
zengfanyu <zengfy43@163.com>
2024-08-22 17:00:21 +0800

    解决git找不到正确路径问题

--- wkcam_R450/source/Applications/git_info/git_info.sh
+++ wkcam_R450/source/Applications/git_info/git_info.sh
@@ -2,6 +2,9 @@
 #if ( [ -d ../.git ] || [ -d ./.git ]) then
 CURR_PATH=`pwd`
 MAIN_PATH=$(dirname `pwd`)
+MAIN_PATH=$(dirname $MAIN_PATH)
+echo "CURR_PATH = $CURR_PATH"
+echo "MAIN_PATH = $MAIN_PATH"
 git config --global --add safe.directory ${MAIN_PATH}
 
 COMMIT_ID=`git log --oneline -n 1 --pretty=format:%h`
@@ -12,6 +15,6 @@ echo "set commit id $COMMIT_ID"
 echo "set commit time $COMMIT_TIME"
 echo "set compiling time $CURR_DATE"
 
-version_info=`cat ${CURR_PATH}/conf_git_info.h | sed "s/RevTag/$COMMIT_ID/g" | sed "s/RevTIME/$COMMIT_TIME/g" | sed "s/RevCOMPLIE/$CURR_DATE/g"`
-echo "$version_info" > ${CURR_PATH}/git_info.h
+version_info=`cat ${CURR_PATH}/Applications/git_info/conf_git_info.h | sed "s/RevTag/$COMMIT_ID/g" | sed "s/RevTIME/$COMMIT_TIME/g" | sed "s/RevCOMPLIE/$CURR_DATE/g"`
+echo "$version_info" > ${CURR_PATH}/Applications/git_info/git_info.h
 #fi
