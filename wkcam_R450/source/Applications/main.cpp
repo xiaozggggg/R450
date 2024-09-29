cc1a268e640121d2f3a5d562de1dee3ee671ec22
huangzhaoshen <978610909@qq.com>
2024-09-29 10:28:02 +0800

    vins-mono方案代码上传。

--- wkcam_R450/source/Applications/main.cpp
+++ wkcam_R450/source/Applications/main.cpp
@@ -9,8 +9,10 @@
 #include <isp_setting.h>
 #include "git_info/git_info.h"
 #include "wk_log.h"
-#include<myslam/visual_odometry.h>
-#include<wkSlam/System.h>
+#include <myslam/visual_odometry.h>
+//#include <wkSlam/System.h>
+#include <vins_mono/vins_estimator/src/estimator_system.h>
+#include "wk_imu_middle.h"
 
 #define TAG "wkcam_app"
 
@@ -51,28 +53,173 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
-	if(true)
-	{
-		WK_SLAM::System SLAM("./release/vc.txt", "./EuRoC.yaml", WK_SLAM::System::MONOCULAR, true);
 
-		wk_st_lk_middle::wk_st_lk_get_instance()->wk_register_get_frame_cb(
-			std::bind(&WK_SLAM::System::TrackMonocular, &SLAM, std::placeholders::_1)
-		);
+	if(argc == 5)
+	{
+		ifstream fImus;
+		fImus.open(argv[4]);
+
+		cv::Mat image;
+		int ni;//num image
+		
+		EstimatorSystem es;
+
+		es.init(argv[1]);
+
+		vector<string> vStrImagesFileNames;
+		vector<double> vTimeStamps;
+		es.LoadImages(string(argv[2]),string(argv[3]),vStrImagesFileNames,vTimeStamps);
+
+		int imageNum = vStrImagesFileNames.size();
+		
+		if(imageNum<=0)
+		{
+			cerr << "ERROR: Failed to load images" << endl;
+			return 1;
+		}
+		
+		es.run();
+
+		for(ni=0; ni<imageNum; ni++)
+		{
+			double  tframe = vTimeStamps[ni];   //timestamp
+			uint32_t  sec = tframe;
+			uint32_t nsec = (tframe-sec)*1e9;
+			nsec = (nsec/1000)*1000+500;
+			ros::Time image_timestamp = ros::Time(sec, nsec);
+			// read imu data
+			es.LoadImus(fImus,image_timestamp);
+
+			//read image from file
+			image = cv::imread(vStrImagesFileNames[ni],CV_LOAD_IMAGE_UNCHANGED);
+
+			if(image.empty())
+			{
+				cerr << endl << "Failed to load image: " << vStrImagesFileNames[ni] <<endl;
+				return 1;
+			}
+
+			cv::resize(image, image, cv::Size(), 0.5, 0.5, cv::INTER_NEAREST);
+
+			std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+			wk_corner_video_frame_s::wk_ptr img_data = std::make_shared<wk_corner_video_frame_s>();
+			es.img_callback(image, image_timestamp, img_data);
+			std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+			double timeSpent =std::chrono::duration_cast<std::chrono::duration<double>>(t2-t1).count();
+
+			//wait to load the next frame image
+			double T=0;
+			if(ni < imageNum-1)
+				T = vTimeStamps[ni+1]-tframe;   //interval time between two consecutive frames,unit:second
+			else if(ni>0)                       //lastest frame
+				T = tframe-vTimeStamps[ni-1];
+
+			if(timeSpent < T)
+				usleep((T-timeSpent)*1e6);      //sec->us:1e6
+			else
+				cerr << endl << "process image speed too slow, larger than interval time between two consecutive frames" << endl;
+		}
 	}
 	else
 	{
-		myslam::VisualOdometry::Ptr vo( new myslam::VisualOdometry() );
+		EstimatorSystem es;
 
-		if(!vo->Init(argv[1]))
+		es.init(argv[1]);
+
+		es.run();
+
+		cv::FileStorage fsSettings(argv[1], cv::FileStorage::READ);
+
+        if(!fsSettings.isOpened())
+            std::cerr << "ERROR: Wrong path to settings " << argv[1] << std::endl;
+
+        double factor =  static_cast<double>(fsSettings["factor"]);
+
+		auto ImgCallBack = [&](wk_corner_video_frame_s::wk_ptr img_data)
 		{
-			std::cout << "vo->Ini() fail!" << std::endl;
-			return -1;
-		}
+			static int tmp1 = 0;
 
-		wk_st_lk_middle::wk_st_lk_get_instance()->wk_register_get_frame_cb(
-			std::bind(&myslam::VisualOdometry::Step, vo, std::placeholders::_1)
-		);
+			cv::Mat im;
+			double timestamp = img_data->pts;
+			timestamp /= 1e6;
+
+			if(tmp1%60==0)
+			{
+				std::cout << "--------ImgCallBack!--------" << std::endl;
+				std::cout << "img_data->pts: " << img_data->pts << " timestamp: " << timestamp << std::endl;
+			}
+
+			tmp1++;
+
+			wk_st_lk_middle::wk_st_lk_get_instance()->wk_frame_to_mat(img_data, im);
+
+			if(tmp1%2000==0 || tmp1==1)
+				cv::imwrite(std::to_string(tmp1) + std::string(".jpg"), im);
+
+			cv::resize(im, im, cv::Size(), factor, factor, cv::INTER_NEAREST);
+
+			uint32_t  sec = timestamp;
+			uint32_t nsec = (timestamp-sec)*1e9;
+			nsec = (nsec/1000)*1000+500;
+			ros::Time image_timestamp = ros::Time(sec, nsec);
+
+			es.img_callback(im, image_timestamp, img_data);
+		};
+
+		auto IMUCallBack = [&](wk_imu_data_s::wk_ptr imu_data)
+		{
+			static int tmp2 = 0;
+
+			double timestamp = imu_data->u64pts;
+			timestamp /= 1e6;
+
+			if(tmp2%200==0)
+			{
+				std::cout << "--------IMUCallBack!--------" << std::endl;
+				std::cout << "imu_data->u64pts: " << imu_data->u64pts << " timestamp: " << timestamp << std::endl;
+			}
+
+			tmp2++;
+
+			sensor_msgs::ImuPtr imudata(new sensor_msgs::Imu);
+			imudata->angular_velocity.x = imu_data->gyro_x;
+			imudata->angular_velocity.y = imu_data->gyro_y;
+			imudata->angular_velocity.z = imu_data->gyro_z;
+			imudata->linear_acceleration.x = imu_data->accel_x;
+			imudata->linear_acceleration.y = imu_data->accel_y;
+			imudata->linear_acceleration.z = imu_data->accel_z;
+			uint32_t  sec = timestamp;
+			uint32_t nsec = (timestamp-sec)*1e9;
+			nsec = (nsec/1000)*1000+500;
+			imudata->header.stamp = ros::Time(sec,nsec);
+			es.imu_callback(imudata);
+		};
+
+		wk_st_lk_middle::wk_st_lk_get_instance()->wk_register_get_frame_cb(ImgCallBack);
+
+		wk_imu_middle::wk_imu_get_instance()->wk_register_get_imu_cb(IMUCallBack);
 	}
+
+#if 0
+	WK_SLAM::System SLAM("./release/vc.txt", "./EuRoC.yaml", WK_SLAM::System::MONOCULAR, true);
+
+	wk_st_lk_middle::wk_st_lk_get_instance()->wk_register_get_frame_cb(
+		std::bind(&WK_SLAM::System::TrackMonocular, &SLAM, std::placeholders::_1)
+	);
+#elif 0
+	myslam::VisualOdometry::Ptr vo( new myslam::VisualOdometry() );
+
+	if(!vo->Init(argv[1]))
+	{
+		std::cout << "vo->Ini() fail!" << std::endl;
+		return -1;
+	}
+
+	wk_st_lk_middle::wk_st_lk_get_instance()->wk_register_get_frame_cb(
+		std::bind(&myslam::VisualOdometry::Step, vo, std::placeholders::_1)
+	);
+#endif
+
 
 	while (1) {
 		//wk_mpp_get_Exptime_gain();
---
+++ wkcam_R450/source/Applications/vins_mono/vins_estimator/src/estimator_system.h
@@ -0,0 +1,71 @@
+#include "estimator.h"
+#include "parameters.h"
+#include "utility/visualization.h"
+#include "loop-closure/loop_closure.h"
+#include "loop-closure/keyframe.h"
+#include "loop-closure/keyframe_database.h"
+#include "camodocal/camera_models/CameraFactory.h"
+#include "camodocal/camera_models/CataCamera.h"
+#include "camodocal/camera_models/PinholeCamera.h"
+/****************** load image section ***********************/
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <thread>
+#include <condition_variable>
+/****************** load image section ***********************/
+
+/****************** feature tracker section ***********************/
+#include "../../include/PointCloud.h"
+#include "../../include/Imu.h"
+#include "feature_tracker/feature_tracker.h"
+#include "wk_corner_info.h"
+
+class EstimatorSystem
+{
+private:
+    using imu_img_type = std::vector<std::pair<std::vector<sensor_msgs::ImuConstPtr>, sensor_msgs::PointCloudConstPtr>>;
+public:
+
+    void run(void);
+
+    void init(const std::string& yamlPath);
+
+    void generateCameraFromYamlFile(void);
+
+    void updateLoopPath(nav_msgs::Path _loop_path);
+
+    void predict(const sensor_msgs::ImuConstPtr &imu_msg);
+
+    void update();
+
+    imu_img_type getMeasurements();
+
+    void imu_callback(const sensor_msgs::ImuConstPtr &imu_msg);
+
+    void feature_callback(const sensor_msgs::PointCloudConstPtr &feature_msg);
+
+    void send_imu(const sensor_msgs::ImuConstPtr &imu_msg);
+
+    //thread:loop detection
+    void process_loop_detection();
+
+    //thread: pose_graph optimization
+    void process_pose_graph();
+
+    // thread: visual-inertial odometry
+    void process();
+
+    void img_callback(const cv::Mat &show_img, const ros::Time &timestamp, wk_corner_video_frame_s::wk_ptr img_data);
+
+    /******************* load image begin ***********************/
+    void LoadImages(const string &strImagePath, const string &strTimesStampsPath, vector<string> &strImagesFileNames, vector<double> &timeStamps);
+    /******************* load image end ***********************/
+
+    /******************* load IMU begin ***********************/
+
+    void LoadImus(ifstream & fImus, const ros::Time &imageTimestamp);
+    /******************* load IMU end ***********************/
+
+    void SendResult(const Vector3d& t, const Matrix3d& r, wk_corner_video_frame_s::wk_ptr img_data, int track_num);
+};
