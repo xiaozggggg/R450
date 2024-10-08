5c5ab0bdcbe2f6c49eeaf18627960d918e985d27
zengfanyu <zengfy43@163.com>
2024-10-08 14:36:16 +0800

    1. 有崩溃风险，添加锁

--- wkcam_R450/source/Applications/quaternion/wk_quaternion_mng.cpp
+++ wkcam_R450/source/Applications/quaternion/wk_quaternion_mng.cpp
@@ -6,6 +6,7 @@ typedef struct {
 	bool b_proc_running;
     pthread_t proc_id;
 
+	pthread_mutex_t mutex;	
 	std::list<wk_quaternion_data_s> q_data;
 }wk_quaternion_info_s;
 
@@ -27,8 +28,11 @@ td_s32 wk_quaternion_push_data(td_float q0, td_float q1, td_float q2, td_float q
 	data.q[2] = q2;
 	data.q[3] = q3;
 	data.u64pts = pts*1000;
-	
+
+	pthread_mutex_lock(&pmng->mutex);
 	pmng->q_data.push_back(data);
+	pthread_mutex_unlock(&pmng->mutex);
+	
 	return TD_SUCCESS;
 }
 
@@ -42,7 +46,10 @@ static void * _wk_quaternion_handle(void* _pArgs)
 			/* 链表数据回调 */
 			wk_quaternion_data_s data = pmng->q_data.front();
 			wk_quaternion_middle::wk_quaternion_get_instance()->wk_quaternion_data_reported(data);
+		
+			pthread_mutex_lock(&pmng->mutex);
 			pmng->q_data.pop_front();
+			pthread_mutex_unlock(&pmng->mutex);
 		}
 		usleep(10*1000);
 	}
@@ -60,16 +67,27 @@ td_s32 wk_quaternion_start()
 		return TD_SUCCESS;
 	}
 
+	ret = pthread_mutex_init(&pmng->mutex, NULL);
+	if(ret != 0) {
+		WK_LOGE("ret is %d\n", ret);
+		goto exit0;
+	}
+
 	std::list<wk_quaternion_data_s>().swap(pmng->q_data);
 	pmng->b_proc_running = TD_TRUE;
 	ret = pthread_create(&pmng->proc_id, NULL, _wk_quaternion_handle, pmng);
     if (ret != 0) {
         WK_LOGE("initial _wk_quaternion_handle thread error\r\n");
-		pmng->b_proc_running = TD_FALSE;
-        return -1;
+        goto exit1;
     }	
 	   
 	return TD_SUCCESS;
+
+exit1:
+	pthread_mutex_destroy(&pmng->mutex);
+	pmng->b_proc_running = TD_FALSE;
+exit0:	
+	return TD_FAILURE;
 }
 
 /* 四元素数据获取功能停止 */
@@ -87,7 +105,8 @@ td_s32 wk_quaternion_stop()
 	    }			
 		pmng->proc_id = 0;
 	}	
-	
+
+	pthread_mutex_destroy(&pmng->mutex);
 	std::list<wk_quaternion_data_s>().swap(pmng->q_data);
 	return TD_SUCCESS;
 }
